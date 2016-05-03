174852881e99af921823f8942bf3153fff30429f
Oliver Dowling <oliver.dowling@gmail.com>
2016-05-03 12:14:12 +1000

    allow generic dota-command to get current scheduled game

--- handlers/handler.game.js
+++ handlers/handler.game.js
@@ -15,7 +15,7 @@ module.exports = function(bot) {
   // *****************************
   //    Game creation / reschedule
   // *****************************
-  bot.onText(/^\/dog?t[ea]s?(?:@\w*)?\b\s*(?=(?:.*\b(\d{2})[:.;-]?(\d{2})\b)?)/i, function(msg, match) {
+  bot.onText(/^\/dog?t[ea]s?(?:@\w*)?\b\s*(?=(?:.*\b(\d{2})[:.;-]?(\d{2})\b)?)(.+)?/i, function(msg, match) {
     winston.info('handler.game - game creation request received');
     var chatId = msg.chat.id;
     var details = match[2];
@@ -25,31 +25,36 @@ module.exports = function(bot) {
       displayName += ' ' + msg.from.last_name;
     }
 
-    if (!match[1] && !match[2]) {
-      // no valid time found, let the user know
+    if (match[1] && match[2]) {
+      var gameTime = new Date();
+      gameTime.setHours(match[1]);
+      gameTime.setHours(match[2]);
+      gameTime.setSeconds(0);
+    } else if (match[3]) {
+      // no valid time found, and there was other text that we do not currently recognise
       return bot.sendMessage(chatId, 'Unrecognised command. Usage example: `/dota at 1730`');
     }
 
-    var gameTime = new Date();
-    if (match[1]) gameTime.setHours(match[1]);
-    if (match[2]) gameTime.setHours(match[2]);
-    gameTime.setSeconds(0);
 
     // Find game
     return Game.findOne({complete: false}).exec()
       .then(game => {
         if(game) {
-          // Update the existing game
-          winston.info(' Game already exists. Updating');
-          game.gameTime = gameTime;
-          game.notified = false;
-          game.shotgun(displayName);
+          if (gameTime) {
+            // Update the existing game
+            winston.info(' Game already exists. Updating');
+            game.gameTime = gameTime;
+            game.notified = false;
+            game.shotgun(displayName);
 
-          winston.info(' saving...');
-          return game.save()
-            .then(saved => bot.sendMessage(chatId, 'Dogte time modified'))
-            .then(sent => game.sendTimeUpdate(bot, chatId));
-        } else {
+            winston.info(' saving...');
+            return game.save()
+              .then(saved => bot.sendMessage(chatId, 'Dogte time modified'))
+              .then(sent => game.sendTimeUpdate(bot, chatId));
+          } else {
+            return game.sendTimeUpdate(bot, chatId);
+          }
+        } else if (gameTime) {
           // Create a new game
           game = new Game({
             gameTime: gameTime,
@@ -59,6 +64,8 @@ module.exports = function(bot) {
           });
           return game.save()
             .then(sent => game.sendTimeUpdate(bot, chatId));
+        } else {
+          bot.sendMessage(chatId, 'No currently scheduled dogte games.');
         }
       }).catch(err => handleError(err, chatId));
   });
