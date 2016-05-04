196a6f8ec5be80d0f0aa67546a1b6c7510284d91
Nick Germyn <ngermyn@ncig.com.au>
2016-05-04 10:09:28 +1000

    Simplified test

--- test/unit/handlers/handler.game.spec.js
+++ test/unit/handlers/handler.game.spec.js
@@ -11,32 +11,19 @@ if (!TOKEN) {
   throw new Error('Bot token not provided');
 }
 
+// This should be extracted into another file and imported
+const sendMessage = (bot, message) => {
+  bot.textRegexpCallbacks.forEach((reg) => {
+    const result = reg.regexp.exec(message.text);
+    if (result) {
+      reg.callback(message, result);
+    }
+  });
+};
+
 describe('handler.game', function gameHandlerSuite() {
   describe('#gameCreation', function gameCreationSuite() {
-    it('should extract the hours and minutes from message "/dota at 1930"', function test(done) {
-      const bot = new TelegramBot(TOKEN, { webHook: true });
-      bot.onText(/^\/dog?t[ea]s?(?:@\w*)?\b\s*(?=(?:.*\b(\d{2})[:.;-]?(\d{2})\b)?)(.+)?/i, (msg, match) => {
-        bot._WebHook._webServer.close();
-        try {
-          expect(match[1]).to.equal('19');
-          expect(match[2]).to.equal('30');
-          done();
-        } catch (e) {
-          done(e);
-        }
-      });
-      const url = `http://localhost:8443/bot${TOKEN}`;
-      request({
-        url,
-        method: 'POST',
-        json: true,
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: { update_id: 0, message: { text: '/dota at 1930' } }
-      });
-    });
-
+    // Example from the node-telegram-bot-api github project
     it('should call `onText` callback on match', function test(done) {
       const bot = new TelegramBot(TOKEN, { webHook: true });
       bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -55,5 +42,21 @@ describe('handler.game', function gameHandlerSuite() {
         body: { update_id: 0, message: { text: '/echo ECHO ALOHA' } }
       });
     });
+
+    // My re-working of their example
+    it('should extract the hours and minutes from message "/dota at 1930"', function test(done) {
+      const bot = new TelegramBot(TOKEN);
+      bot.onText(/^\/dog?t[ea]s?(?:@\w*)?\b\s*(?=(?:.*\b(\d{2})[:.;-]?(\d{2})\b)?)(.+)?/i, (msg, match) => {
+        try {
+          expect(match[1]).to.equal('19');
+          expect(match[2]).to.equal('30');
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+      sendMessage(bot, { text: '/dota at 1930' });
+    });
+
   });
 });
