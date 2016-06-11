aa2092a4aa87a6cec019ae965a320339a544d8df
Oliver Dowling <oliver.dowling@gmail.com>
2016-06-11 14:15:45 +1000

    spaces to tabs

--- test/mocks.js
+++ test/mocks.js
@@ -4,39 +4,39 @@ var Promise = require('bluebird');
 
 /* Mock of the TelegramBot */
 class BotMock {
-  constructor() {
-    this.registeredCallbacks = [];
-    this.sentMessages = [];
-  }
+	constructor() {
+		this.registeredCallbacks = [];
+		this.sentMessages = [];
+	}
 
-  onText(regexp, callback) {
-    this.registeredCallbacks.push({ regexp, callback });
-  }
+	onText(regexp, callback) {
+		this.registeredCallbacks.push({ regexp, callback });
+	}
 
-  simulateMessage(message) {
-    const promises = [];
-    this.registeredCallbacks.forEach(reg => {
-      const result = reg.regexp.exec(message.text);
-      if (result) {
-        promises.push(reg.callback(message, result));
-      }
-    });
-    return Promise.all(promises);
-  }
+	simulateMessage(message) {
+		const promises = [];
+		this.registeredCallbacks.forEach(reg => {
+			const result = reg.regexp.exec(message.text);
+			if (result) {
+				promises.push(reg.callback(message, result));
+			}
+		});
+		return Promise.all(promises);
+	}
 
-  sendMessage(chatId, message, options) {
-    var self = this;
-    return new Promise((resolve, reject) => {
-      self.sentMessages.push({
-        chatId: chatId,
-        message: message,
-        options: options
-      });
-      resolve();
-    });
-  }
+	sendMessage(chatId, message, options) {
+		var self = this;
+		return new Promise((resolve, reject) => {
+			self.sentMessages.push({
+				chatId: chatId,
+				message: message,
+				options: options
+			});
+			resolve();
+		});
+	}
 }
 
 module.exports = {
-  Bot: BotMock
+	Bot: BotMock
 };
